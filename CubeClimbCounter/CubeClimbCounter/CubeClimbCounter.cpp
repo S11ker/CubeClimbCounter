edea4df171fce241ecf706312cb0c0acafffdc7c
S11ker <richard.saifier.87@gmail.com>
2023-10-09 23:14:42 +0300

    Update main function declaration to accept command line arguments
    Updated main function to accept 'argc' and 'argv' parameters for command line execution.

--- CubeClimbCounter/CubeClimbCounter/CubeClimbCounter.cpp
+++ CubeClimbCounter/CubeClimbCounter/CubeClimbCounter.cpp
@@ -2,7 +2,7 @@
 #include "CubeClimbFunction.h"
 #include "WorkWithIFDInTxtFile.h"
 
-int main()
+int main(const int argc, char** argv)
 {
     int stepsResult = cubeStepsCounter(100, 101);
     cout << stepsResult << endl;
