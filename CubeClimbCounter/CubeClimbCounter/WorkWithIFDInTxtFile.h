f41c97e68c863ca5b24e3e4382993838403c940a
S11ker <richard.saifier.87@gmail.com>
2023-10-09 10:29:44 +0300

    Add extractIFDFromTxt function
    The function extracts a number from a text file (.txt), while checking for correctness and determining its type (int, float or double). If everything is successful, the function returns the received number, otherwise it returns an exception.

--- CubeClimbCounter/CubeClimbCounter/WorkWithIFDInTxtFile.h
+++ CubeClimbCounter/CubeClimbCounter/WorkWithIFDInTxtFile.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <fstream>
+#include <string>
+#include <regex>
+#include <variant>
+#include <stdexcept>
+using namespace std;
+
+variant<int, float, double> extractIFDFromTxt(const string& fileName) {
+
+    
+}
