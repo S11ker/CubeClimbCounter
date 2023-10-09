f364719408534b608122e10ca566cd3d3eb97f2c
S11ker <richard.saifier.87@gmail.com>
2023-10-09 10:32:38 +0300

    Add body of extractIFDFromTxt function

--- CubeClimbCounter/CubeClimbCounter/WorkWithIFDInTxtFile.h
+++ CubeClimbCounter/CubeClimbCounter/WorkWithIFDInTxtFile.h
@@ -8,5 +8,41 @@ using namespace std;
 
 variant<int, float, double> extractIFDFromTxt(const string& fileName) {
 
-    
+    if (fileName.find(".txt") == string::npos) {
+        throw runtime_error("Invalid file format");
+    }
+
+    locale::global(locale("en_US.UTF-8"));
+
+    ifstream file(fileName);
+
+    if (!file.is_open()) {
+
+        throw runtime_error("Could not open the file");
+    }
+
+    string line;
+    if (getline(file, line)) {
+
+        regex intRegex("\\d+");
+        regex floatRegex("\\d+\\.\\d");
+        regex doubleRegex("\\d+\\.\\d+");
+
+        if (regex_match(line, intRegex)) {
+            return stoi(line);
+        }
+        else if (regex_match(line, floatRegex)) {
+            return stof(line);
+        }
+        else if (regex_match(line, doubleRegex)) {
+
+            return stod(line);
+        }
+        else {
+            throw runtime_error("It is impossible to determine the type of number in a string");
+        }
+    }
+    else {
+        throw runtime_error("File is empty");
+    }
 }
