d1c7b90a46fcaf79de81ce656b54d041e7e1f04d
S11ker <richard.saifier.87@gmail.com>
2023-10-14 18:04:29 +0300

    Add test file for extractIFDFromTxt function

---
+++ CubeClimbCounter/extractIFDFromTxtTests/extractIFDFromTxtTests.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../CubeClimbCounter/WorkWithIFDInTxtFile.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace extractIFDFromTxtTests
+{
+	TEST_CLASS(extractIFDFromTxtTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
