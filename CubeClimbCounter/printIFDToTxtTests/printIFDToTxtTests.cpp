103db9e633ce039b3fb0bc2225e253e93531630b
S11ker <richard.saifier.87@gmail.com>
2023-10-15 23:34:07 +0300

    Add test file for printIFDToTxt function

---
+++ CubeClimbCounter/printIFDToTxtTests/printIFDToTxtTests.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../CubeClimbCounter/WorkWithIFDInTxtFile.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace printIFDToTxtTests
+{
+	TEST_CLASS(printIFDToTxtTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
