0aa61b847d35fb944b39e4255b981633d197bc65
S11ker <richard.saifier.87@gmail.com>
2023-10-15 23:24:28 +0300

    Add test file for cubeStepsCounter function

---
+++ CubeClimbCounter/cubeStepsCounterTests/cubeStepsCounterTests.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../CubeClimbCounter/CubeClimbFunction.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace cubeStepsCounterTests
+{
+	TEST_CLASS(cubeStepsCounterTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
