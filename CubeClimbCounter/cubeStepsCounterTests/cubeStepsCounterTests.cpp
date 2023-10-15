3b044ad3f4bda5dd7370dfb0f367cc013f4c4b1d
S11ker <richard.saifier.87@gmail.com>
2023-10-15 23:31:43 +0300

    Add all tests for cubeStepsCounter function

--- CubeClimbCounter/cubeStepsCounterTests/cubeStepsCounterTests.cpp
+++ CubeClimbCounter/cubeStepsCounterTests/cubeStepsCounterTests.cpp
@@ -10,8 +10,32 @@ namespace cubeStepsCounterTests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(creatingStepsWitnMinNumberOfCubes)
 		{
+			int stepsResult = cubeStepsCounter(1, 2);
+			int expStepsResult = 1;
+			Assert::AreEqual(expStepsResult, stepsResult);
+		}
+
+		TEST_METHOD(creatingStepsWitnMeanNumberOfCubes)
+		{
+			int stepsResult = cubeStepsCounter(50, 51);
+			int expStepsResult = 3658;
+			Assert::AreEqual(expStepsResult, stepsResult);
+		}
+
+		TEST_METHOD(creatingStepsWitnMaxNumberOfCubes)
+		{
+			int stepsResult = cubeStepsCounter(100, 101);
+			int expStepsResult = 444793;
+			Assert::AreEqual(expStepsResult, stepsResult);
+		}
+
+		TEST_METHOD(creatingStepsWitnNoNumberOfCubes)
+		{
+			int stepsResult = cubeStepsCounter(0, 1);
+			int expStepsResult = 1;
+			Assert::AreEqual(expStepsResult, stepsResult);
 		}
 	};
 }
